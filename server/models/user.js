b3423891d1ff2ce02b55ef33aaa8488b1cd275ff
Nick <tnikos28@gmail.com>
2023-10-26 11:07:39 +0300

    added user model and tested user creation from POSTMAN

---
+++ server/models/user.js
@@ -0,0 +1,42 @@
+import mongoose from "mongoose";
+
+const userSchema = mongoose.Schema ({
+    firstName:{
+        type:String,
+        required:true,
+        max:12,
+    },
+    lastName:{
+        type:String,
+        required:true,
+        max:12
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    password:{
+        type:String,
+        min:8,
+        required:true
+    },
+    joined:{
+        type:Date,
+        default: new Date()
+    },
+    token:{
+        type:String,
+        default:''
+    },
+     username:{
+        type:String,
+        required:true,
+        min:6,
+        max:15
+     }
+}, {timestamps:true})
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
