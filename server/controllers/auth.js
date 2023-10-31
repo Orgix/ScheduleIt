b48a0d494390f5d3499c7a3149bcfe16277ae668
Nick <tnikos28@gmail.com>
2023-10-31 10:36:31 +0200

    sign in controller completed and tested

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,7 +1,39 @@
-import User from '../models/user.js'
+import  jwt  from "jsonwebtoken";
+import bcrypt from "bcryptjs";
+import mongoose from "mongoose";
+import User from "../models/user.js";
+import { ExpressError } from "../middleware/errHandle.js";
+
 
 export const signIn = async (req,res)=>{
     const { email, pwd} = req.body
+
+    //lookup user in database. if not found, send a 404 code
+    const foundUser = await User.findOne({email}).populate({path:'friends', select: 'firstName lastName'}).exec();
+    if(!foundUser) return res.status(404).json({message: 'User does not exist.'})
+    
+    //if found, compare the password with the hash. 
+    const match = await bcrypt.compare(password, foundUser.password)
+    if(!match) return res.status(401).json({message:'Incorrect credentials'})
+
+    //if it's valid, generate a jwt token(1day length)
+    const accessToken = jwt.sign(
+        {
+            "UserInfo": {
+                "username": foundUser.username,
+                "email": foundUser.email,
+                "firstName": foundUser.firstName,
+                "lastName":foundUser.lastName,
+                "joined" : foundUser.joined,
+                "id": foundUser._id
+            }
+        },
+        process.env.JWT_SECRET,
+        { expiresIn: '1d' }
+    );
+    //before sending the response, handle saving the token, so the updatedAt field gets updated. this will basically be done likewisely on the sync feature
+    //set the user field to that token.
+    foundUser.token = accessToken
 }
 
 export const signUp = async (req,res)=>{
