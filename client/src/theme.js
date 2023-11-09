3a078687e74955b4ae8f3ba3db22212b175fbe60
Nick <tnikos28@gmail.com>
2023-11-09 11:51:17 +0200

    added basic theme colors

---
+++ client/src/theme.js
@@ -0,0 +1,56 @@
+// mui theme settings
+export const themeSettings =  {
+    palette:{
+      primary:{
+        main:'#F98B07',
+        title:'#011213'
+      },
+      background:{
+        default:'#fafafa',
+        dark: '#1E375A'
+      },
+      sidenav:{
+        default:'#afd7e4',
+        letters:'#ababab'
+      },
+      success:{
+        //#071318 120 187 106
+        main:'#F98B07',
+        letters:'#fffefe',
+        buttons: '#78BB66'
+      },
+    },
+    typography:{
+      fontFamily: 'Quicksand',
+      fontWeightLight : 400,
+      fontWeightRegular : 500,
+      fontWeightMedium : 600,
+      fontWeightBold : 700,
+      fontSize:13,
+      h1: {
+        fontFamily: ["Quicksand", "sans-serif"].join(","),
+        fontSize: 40,
+      },
+      h2: {
+        fontFamily: ["Quicksand", "sans-serif"].join(","),
+        fontSize: 32,
+      },
+      h3: {
+        fontFamily: ["Quicksand", "sans-serif"].join(","),
+        fontSize: 24,
+      },
+      h4: {
+        fontFamily: ["Quicksand", "sans-serif"].join(","),
+        fontSize: 20,
+      },
+      h5: {
+        fontFamily: ["Quicksand", "sans-serif"].join(","),
+        fontSize: 16,
+      },
+      h6: {
+        fontFamily: ["Quicksand", "sans-serif"].join(","),
+        fontSize: 14,
+      },
+    }
+};
+  
