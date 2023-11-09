c4e9bb51c7dc3858ecc494fa0680a45131d12d34
Nick <tnikos28@gmail.com>
2023-11-09 11:51:03 +0200

    added theme structure and routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,28 @@
 import { useState } from 'react'
+import React from 'react';
+import Navbar from './components/navbar/Navbar';
+import Home from './pages/Home';
+import {BrowserRouter, Routes, Route, Navigate} from 'react-router-dom'
+import { Container, CssBaseline } from '@mui/material';
+import { useSelector } from 'react-redux';
+import { createTheme, ThemeProvider } from '@mui/material';
+import { themeSettings } from './theme';
+
+const theme  = createTheme(themeSettings)
 
 function App() {
   return (
-    <>
-      <div>
-        
-      </div>
-    </>
+    <ThemeProvider theme={theme}>
+    <BrowserRouter>
+        <CssBaseline/>
+        <Container maxWidth={false} disableGutters>
+          <Navbar color="primary"/>
+          <Routes>
+            <Route  index element={<Home/>} />
+          </Routes>
+        </Container>
+    </BrowserRouter> 
+    </ThemeProvider>
   )
 }
 
