b1950266a6affbf8a32de42c53aba2c0f4d025eb
Nick <tnikos28@gmail.com>
2023-11-24 12:42:12 +0200

    completed base template of the Home component

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,34 +1,48 @@
 import React from 'react'
-import { Container,  Button, Typography,Box } from '@mui/material'
+import { Container,  Button, Typography,Box,Grid } from '@mui/material'
 import { Link } from 'react-router-dom'
 import {LandingStyles as styles} from './styles'
 import background from '../../assets/images/worksp.jpg'
 const Home = () => {
   return (
-    <Box sx={{...styles.outerContainer, backgroundImage: `url(${background})`}}>
-    <Container sx={styles.container}>
+    <Box sx={{...styles.outerContainer,background:'rgba(0,0,0,0.6)'}}>
+      <img src={background} style={styles.containersImage} />
+      <Grid container sx={{}}>
+      
+        <Grid item xs={12} sm={7} lg={6} alignItems="center" justifyContent="center" sx={{mt:5,mb:5}}>
+        
+      <Container sx={styles.container}>
          <>
-          <Typography variant="h4" sx={styles.welcomeMessage}>
+         <Typography variant="h4" sx={{...styles.welcomeMessage, fontSize:'26px'}}>
             Welcome to ScheduleIt!
           </Typography>
           <Typography variant="body1" sx={styles.appDescription}>
-            ScheduleIt is an innovative scheduling platform that helps you efficiently manage and organize your team's schedules. Easily create, assign, and track schedules with our user-friendly interface. With ScheduleIt, you can:
+            ScheduleIt is an innovative scheduling platform that elevates your scheduling experience. Our platform is designed with inspiration drawn from military efficiency, offering a dynamic solution for seamless shift delegation and rapid schedule creation. Whether you're a military professional or managing schedules in a civilian setting, ScheduleIt is tailored to meet your unique needs. Experience the power of efficient time management and stay tuned for exciting developments as we continuously enhance and expand ScheduleIt to redefine the way schedules are crafted.
           </Typography>
-          <ul sx={styles.list}>
-              <li sx={styles.listItem}>Optimize employee scheduling based on preferences</li>
-              <li sx={styles.listItem}>Ensure efficient coverage for different time slots</li>
-              <li sx={styles.listItem}>Handle multiple shift durations and specialties</li>
-              <li sx={styles.listItem}>Provide a seamless experience for both managers and employees</li>
-            </ul>
+          <ul style={styles.list}>
+            <li style={styles.listItem}><Typography fontSize='1.25rem'>Optimize employee scheduling based on preferences</Typography></li>
+            <li style={styles.listItem}><Typography fontSize='1.25rem'>Ensure efficient coverage for different time slots</Typography></li>
+            <li style={styles.listItem}><Typography fontSize='1.25rem'>Handle multiple shift durations and specialties</Typography></li>
+            <li style={styles.listItem}><Typography fontSize='1.25rem'>Provide a seamless experience for both managers and employees</Typography></li>
+          </ul>
           <Typography variant="h4" sx={styles.welcomeMessage}>
             Join ScheduleIt today and revolutionize the way you manage schedules!
           </Typography>
         </>
-    </Container>
-    <Box sx={styles.buttonContainer}>
-<Button variant="contained" color="primary" sx={styles.getStartedButton}>
-    Get Started
-  </Button>
+        </Container>
+        </Grid>
+        <Grid item xs={12} sm={5} lg={6} alignItems="center" justifyContent="center" sx={{mt:5}}>
+        <Box sx={styles.buttonContainer}>
+        <Button variant="contained" color="primary" sx={styles.getStartedButton}>
+          Get Started
+        </Button>
+      </Box>
+        </Grid>
+    </Grid>
+    <Box sx={styles.footContainer}>
+      <Typography variant="body1">
+        © 2023 ScheduleIt. All rights reserved. | Terms of Service | Privacy Policy
+      </Typography>
     </Box>
     </Box>
     
