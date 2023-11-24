db6ea5a1010728108079a4e1594e1aae32427ffc
Nick <tnikos28@gmail.com>
2023-11-24 12:42:50 +0200

    :rocket: basic template CSS for the component

--- client/src/pages/Home/styles.js
+++ client/src/pages/Home/styles.js
@@ -1,30 +1,33 @@
-// styles.js
-
 export const LandingStyles = {
     outerContainer:{
+        position:'relative',
         minHeight:'100vh',
         width:'100%',
+        height:'100%s',
         backgroundSize:'cover',
-        backgroundRepeat: 'no-repeat'
+        backgroundRepeat: 'no-repeat',
     },
     container: {
       display: 'flex',
       flexDirection: 'column',
       alignItems: 'center',
       justifyContent: 'center',
-      color:'white'
+      color:'white',
     },
     welcomeMessage: {
       marginTop:'10px',
       marginBottom: '1rem',
       textAlign: 'center',
+      color:'white'
     },
     appDescription: {
       marginBottom: '1.5rem',
       textAlign: 'center',
+      fontSize:'1.4rem'
     },
     list: {
       padding: '0',
+      
     },
     listItem: {
       marginBottom: '0.5rem',
@@ -35,14 +38,14 @@ export const LandingStyles = {
         content: '""',
         width: '8px',
         height: '8px',
-        backgroundColor: '#2196F3', // You can customize the color
+        backgroundColor: '#2196F3',
         borderRadius: '50%',
         display: 'inline-block',
         position: 'absolute',
         left: '-1.5rem',
         top: '50%',
         transform: 'translateY(-50%)',
-      },
+      }
     },
     getStartedButton: {
       alignSelf: 'center',
@@ -51,6 +54,23 @@ export const LandingStyles = {
         marginTop:'140px',
         display:'flex',
         flexDirection:'row',
-        justifyContent:'center'
+        justifyContent:'center',
+        alignItems:'center'
     },
-  };
+    containersImage:{
+      width: '100%',
+      height: '100%',
+      position: 'absolute',
+      top: 0,
+      left: 0,
+      zIndex:-1
+    },
+    footContainer:{
+      minHeight: '150px', // Adjust height as needed
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    color: '#fff', // Text color
+    textAlign: 'center',
+    }
+  }
