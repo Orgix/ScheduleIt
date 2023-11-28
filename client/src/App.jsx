b8ed09c4aab10ff2c61acba7492c4b5a576d6016
Nick <tnikos28@gmail.com>
2023-11-28 12:21:57 +0200

    added pages in Routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,9 @@ import { useState } from 'react'
 import React from 'react';
 import Navbar from './components/navbar/Navbar';
 import Home from './pages/Home/Home';
+import Employees from './pages/Employees/Employees';
+import Profile from './pages/Profile/Profile';
+import Schedules from './pages/Schedules/Schedules'
 import {BrowserRouter, Routes, Route, Navigate} from 'react-router-dom'
 import { Container, CssBaseline } from '@mui/material';
 import { useSelector } from 'react-redux';
@@ -19,6 +22,12 @@ function App() {
           <Navbar color="primary"/>
           <Routes>
             <Route  index element={<Home/>} />
+            <Route path="me">
+              {/* There will be an index route here, a different component */}
+              <Route index element={<Profile/>}/>
+              <Route path="employeeList" element={<Employees/>}/>
+              <Route path="schedules" element={<Schedules/>} />
+            </Route>
           </Routes>
         </Container>
     </BrowserRouter> 
