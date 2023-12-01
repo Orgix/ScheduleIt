6dd4c6686be99371543452313856bb50e1a17093
Nick <tnikos28@gmail.com>
2023-12-01 15:12:25 +0200

    converted error handler to a middleware

--- server/middleware/errHandle.js
+++ server/middleware/errHandle.js
@@ -1,7 +1,12 @@
-export class ExpressError extends Error{
-    constructor(message,statusCode){
-        super();
-        this.message = message;
-        this.statusCode = statusCode
-    }
+import { logEvents } from "./logger"
+
+export const errorHandler = (err, req, res, next) => {
+    logEvents(`${err.name}: ${err.message}\t${req.method}\t${req.url}\t${req.headers.origin}`, 'errLog.log')
+    console.log(err.stack)
+
+    const status = res.statusCode ? res.statusCode : 500 // server error 
+
+    res.status(status)
+
+    res.json({ message: err.message })
 }
