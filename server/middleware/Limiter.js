c948841533e552db147c8e619e2922e448f05dab
Nick <tnikos28@gmail.com>
2023-12-01 15:12:41 +0200

    added rate Limiter to middleware

---
+++ server/middleware/Limiter.js
@@ -0,0 +1,15 @@
+import rateLimit from 'express-rate-limit'
+import { logEvents } from './logger'
+
+export const limiter = rateLimit({
+    windowMs: 60*1000,
+    max: 5,
+    message:
+    { message:'This ip has made the max attempts. Please attempt after a 60 second pause.'},
+        handler:(req,res,next,options)=>{
+            logEvents(`Number of max Requests exceeded: ${options.message.message}\t${req.method}\t${req.url}\t${req.headers.origin}`,`errorLog.log`)
+            res.status(options.statusCode).send(options.message)
+        },
+        standardHeaders: true,
+        legacyHeaders: false,
+})
