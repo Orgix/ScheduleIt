8c5750e124242b91d5ce11eb62616cd2188dcbb3
Nick <tnikos28@gmail.com>
2023-12-04 16:57:11 +0200

    modified general store

--- client/src/services/state/store.js
+++ client/src/services/state/store.js
@@ -1,8 +1,14 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit'
 import authSlice from './authSlice'
+import { apiSlice } from '../api/apiSlice'
+import { setupListeners } from '@reduxjs/toolkit/query'
 
 export const store = configureStore({
   reducer: {
+    [apiSlice.reducerPath]: apiSlice.reducer,
     auth: authSlice,
   },
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware().concat(apiSlice.middleware),
+    devTools:true
 })
