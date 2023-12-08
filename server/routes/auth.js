02ba300139b5abf8402eeaa48a35b00419cd14bb
Nick <tnikos28@gmail.com>
2023-12-08 15:11:18 +0200

    modified existing route

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,12 +1,12 @@
 import express from "express";
-import { signIn, signUp, signout } from "../controllers/auth.js";
+import { signIn, signUp, logout } from "../controllers/auth.js";
 import { limiter } from "../middleware/Limiter.js";
 
 const router = express.Router();
 
 router.post('/signin',limiter, signIn)
 router.post('/signup',signUp)
-router.post('/signout', signout)
+router.post('/signout', logout)
 router.get('/refresh',)
 
 export default router;
