6d179322c61f07aae6de4fd32ab164fc08584576
Nick <tnikos28@gmail.com>
2023-12-14 23:08:56 +0200

    added schedule controllers

--- server/controllers/schedule.js
+++ server/controllers/schedule.js
@@ -1,6 +1,21 @@
 import Schedule from "../models/schedule.js";
 import { ExpressError } from "../middleware/errHandle.js";
 
-export const fetchSchedules =async (req,res) =>{
-    
+//fetch specific schedule
+export const fetchSchedule =async (req,res) =>{
+    //fetch id from params
+    const {id} = req.params;
+
+    //fetch schedule
+    const schedule = await Schedule.findOne({id});
+
+}
+//fetch all user specific schedules
+export const fetchSchedules = async(req,res)=>{
+     //fetch id from params
+     const {userId} = req.params;
+
+     //fetch all user schedules
+     const schedule = await Schedule.find({user: userId});
+ 
 }
